5a5402a098c79a34119d7d93824fd549f9c385ae
lgoldstein <lgoldstein@groupon.com>
2016-03-25 15:48:45 -0700

    migrations

--- db/migrate/20160325203224_add_users_table.rb
+++
@@ -1,4 +0,0 @@
-class AddUsersTable < ActiveRecord::Migration
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325203716) do
+ActiveRecord::Schema.define(version: 20150101170206) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -26,6 +24,9 @@ ActiveRecord::Schema.define(version: 20160325203716) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
